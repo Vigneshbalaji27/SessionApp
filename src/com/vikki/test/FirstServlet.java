7157f94251426c58112bf564708fc41be61a6626
Vigneshbalaji27 <vigneshbalajikv@gmail.com>
2017-12-13 23:19:42 +0530

    session app

---
+++ src/com/vikki/test/FirstServlet.java
@@ -0,0 +1,51 @@
+package com.vikki.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside doget() of FS");
+		String name=request.getParameter("uname");
+		System.out.println("name from req = "+name);
+		
+		HttpSession s=request.getSession(true);
+		s.setAttribute("user", name);
+		s.setAttribute("num",(int) (Math.random()*100));
+		
+		String ht="<html><body><b> Session Created and name stored</b></body></html> ";
+		PrintWriter pw=response.getWriter();
+		pw.println(ht);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/vikki/test/SecondServlet.java
@@ -0,0 +1,55 @@
+package com.vikki.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside doget() of SS");
+		
+		HttpSession s=request.getSession(false);
+		PrintWriter out=response.getWriter();
+		if(s==null)
+		{
+			out.println("<b>Your Session does not exists.login first and then send me request </b>");
+		}
+		else
+		{
+		String data=(String)s.getAttribute("user");
+		int i=(Integer)s.getAttribute("num");
+		
+		out.println("<b> Data from session name = "+data +" num = "+i+"</b><br/>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
